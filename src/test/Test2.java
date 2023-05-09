c2acedda0cbc3240a1eb2d9c6f8cbbffe723121f
Leedius <tiuy2750@gmail.com>
2023-05-09 15:11:33 +0900

    강의실 추가 작업

---
+++ src/test/Test2.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Test2 {
+	public static void main(String[] ar) {
+		System.out.println("프로그램 시작");
+		System.out.println("프로그램 끝");
+		System.out.println("집에서 작업함");
+	}
+}
